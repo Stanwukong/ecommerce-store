f591a7a8b59e2cab0261d0f72d6b3b314dd11eba
WuKong <111810384+Stanwukong@users.noreply.github.com>
2023-09-27 08:47:02 +0100

    Added dep and improved button component

--- components/navbar-actions.tsx
+++ components/navbar-actions.tsx
@@ -1,11 +1,33 @@
 "use client"
 
 import Button from "@/components/ui/button";
+import { ShoppingBag } from "lucide-react";
+import { useEffect, useState } from "react";
 
 const NavbarActions = () => {
+
+	const [mounted, setMounted] = useState(false);
+
+	useEffect(() => {
+		setMounted(true);
+	}, []);
+
+	if (!mounted) return null;
+
+
 	return ( 
 		<div className="ml-auto flex items-center gap-x-4">
-			<Button></Button>
+			<Button
+				className="flex items-center rounded-full px-4 py-2"
+			>
+				<ShoppingBag 
+					size={20}
+					color="white"
+				/>
+				<span className="ml-2 text-sm font-medium">
+					0
+				</span>
+			</Button>
 		</div>
 	 );
 }
