1922071ce688bd569b8cb08de79637cf54868a05
WuKong <111810384+Stanwukong@users.noreply.github.com>
2023-09-27 09:12:40 +0100

    Added getBillboard action, setup billboard banner

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,6 +1,17 @@
-const HomePage = () => {
+import getBillboard from "@/actions/get-billboard";
+import Billboard from "@/components/billboard";
+import Container from "@/components/ui/container";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+	const billBoard = await getBillboard('7aa0c45d-19d9-48dd-aa7e-a52b4bb007a2');
 	return ( 
-		<div>Hello Store</div>
+		<Container>
+			<div className="space-y-10 pb-10">
+				<Billboard data={billBoard} />
+			</div>
+		</Container>
 	 );
 }
  
