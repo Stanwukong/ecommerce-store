c265ba8af60d8ffd6ad39d05370af0a3a25de7cd
WuKong <111810384+Stanwukong@users.noreply.github.com>
2023-09-29 01:34:13 +0100

    Setup individual product page

--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -5,12 +5,18 @@ import Image from "next/image"
 import IconButton from "@/components/ui/icon-button"
 import { Expand, ShoppingCart } from "lucide-react"
 import Currency from "@/components/ui/currency"
+import { useRouter } from "next/navigation"
 
 interface ProductCardProps {
   data: Product
 }
 
 const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
+	const router = useRouter();
+
+	const handleClick = () => {
+		router.push(`/product/${data.id}`)
+	}
   return (
     <div className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
       {/* {Images and Actions} */}
@@ -24,7 +30,7 @@ const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
         <div className="opacity-0 group-hover:opacity-100 transition absolute w-full px-6 bottom-5">
           <div className="flex gap-x-6 justify-center">
 			<IconButton 
-				onClick={() => {}}
+				onClick={handleClick}
 				icon={<Expand size={20} className="text-gray-600"/>}
 			/>
 			<IconButton 
